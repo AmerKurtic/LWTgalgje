42bdb30328326d4a2a02594d04361bdb8e4d499f
AmerKurtic <amerkurtic@gmail.com>
2015-05-15 11:24:30 +0200

    first commit hoi

--- dbconnect.php
+++ dbconnect.php
@@ -2,6 +2,29 @@
 
 //192.168.245.208
 
+$serverIP = "192.168.245.208";
+$username = "Training01";
+$password = "Training01!";
+$db = "galgje";
+
+$con = new mysqli($serverIP,$username,$password,$db);
+
+if($con->connect_error)
+{
+    die("Connection failed: " . $con->connect_error);
+}
+
+$sql = "SELECT `woord` from `woorden` WHERE `wID` = '1'";
+$result = $con->query($sql);
+
+if($result->num_rows > 0)
+{
+    while($row = $result->fetch_assoc())
+    {
+        echo "woord: ". $row["woord"];
+    }
+}
+
 function getRandomWord()
 {}
 
@@ -11,6 +34,6 @@ function checkLetter()
 function addLetter()
 {}
 
-function reset()
+function resetDB()
 {}
 
