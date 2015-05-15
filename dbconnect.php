e1b01b7d288f127e2d32a9a997cc572b47f3eba6
AmerKurtic <amerkurtic@gmail.com>
2015-05-15 13:08:56 +0200

    first commit hoi

--- dbconnect.php
+++ dbconnect.php
@@ -14,32 +14,100 @@ if($con->connect_error)
     die("Connection failed: " . $con->connect_error);
 }
 
-function getRandomWord()
+class DB
 {
-    global $con;
-    $sql = "SELECT `woord` from `woorden` ORDER BY RAND() LIMIT 1";
-    $result = $con->query($sql);
-
-    if($result->num_rows > 0)
+    function getRandomWord()
     {
-        while($row = $result->fetch_assoc())
-        {
-            return $row["woord"];
+        global $con;
+        $sql = "SELECT * from `woorden` ORDER BY RAND() LIMIT 1";
+        $result = $con->query($sql);
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc())
+            {
+                return array($row["wID"],$row["woord"]);
+            }
         }
     }
+
+    function getuID($ip)
+    {
+        global $con;
+        $ip = mysqli_real_escape_string($con,$ip);
+        $sql = "SELECT * from `users` WHERE `ip` = '$ip'";
+        $result = $con->query($sql);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                return $row["uID"];
+            }
+        }
+        else
+        {
+            $sql = "INSERT INTO `users` (`ip`) VALUES ('$ip')";
+            if($con->query($sql))
+            {
+                return $this->getuID($ip);
+            }
+            else
+            {
+                die("Connection error: ". $con->error);
+            }
+        }
+    }
+
+    function getGameWID($uID)
+    {
+        global $con;
+        $sql = "SELECT * from `game` WHERE `uID` = '$uID'";
+        $result = $con->query($sql);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                return $row["wID"];
+            }
+        }
+        else
+        {
+
+        }
+    }
+
+    function checkGame($uID)
+    {
+        global $con;
+        $sql = "SELECT * from `game` WHERE `uID` = '$uID'";
+        $result = $con->query($sql);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                return true;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    function newGame($uID,$wID)
+    {
+        global $con;
+        $sql = "INSERT INTO `game` (`uID`,`wID`) VALUES ('$uID','$wID')";
+        if($con->query($sql))
+        {   return true;
+        }
+        else
+        {
+            die("Connection error: ". $con->error);
+        }
+    }
+
+    function checkLetter($string)
+    {
+        global $con;
+        $sql = "SELECT";
+    }
+
+    function resetDB()
+    {
+    }
+
 }
-
-function getuID($ip)
-{
-
-}
-
-function checkLetter($string)
-{
-    global $con;
-    $sql = "SELECT";
-}
-
-function resetDB()
-{}
-
--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <?php
-
 include("draw.php");
 include("dbconnect.php");
 
-echo $_SERVER['REMOTE_ADDR'];
+$letters = array("a","b",);
+
+$uIP = $_SERVER["REMOTE_ADDR"];
+
+$db = new DB;
+
+$uID = $db->getuID($uIP);
+$wID = "";
+
+if($db->checkGame($uID))
+{
+    $wID = $db->getGameWID($uID);
+}
+else
+{
+    $wID = $db->getRandomWord()[0];;
+    $db->newGame($uID,$wID);
+}
+
+echo $wID;
 ?>
+
