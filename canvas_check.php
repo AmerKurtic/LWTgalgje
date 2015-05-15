69046cdebe707a6395a372a8325281fcae3f0fe9
Mark Meijerman <contact@markmeijerman.nl>
2015-05-15 11:20:38 +0200

    hallo php

--- canvas_check.php
+++ canvas_check.php
@@ -1,7 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: TrainingUser
- * Date: 15-5-2015
- * Time: 11:03sssss
- */
+echo('<DOCTYPE html>
+<html>
+<head>
+<title> canvas test</title>
+<script src="OSC.js"></script>
+</head>
+<body>
+<canvas id="test" width="320" height="240">
+This is a canvas element met id <i>test</i>
+</canvas>
+<script>
+    canvas = 0("test")
+    context = canvas.getContext("2d")
+    context.fillStyle = "red"
+    S(canvas).border = "1px solid black"
+
+    context.beginPath()
+    context.moveTo(160, 120)
+    context.arc(160, 120, 70, 0, Math.PI * 2, false)
+    context.closePath()
+    context.fill()
+</script>
+</body>
+</html>');
+?>
