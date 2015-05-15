b70f652305d524b9e9db9cf4d62b2187ce5ba38c
Mark Meijerman <contact@markmeijerman.nl>
2015-05-15 13:24:31 +0200

    hallo php

--- canvas_check.php
+++ canvas_check.php
@@ -42,15 +42,82 @@ function draw($i)
                 $draw .= draw($count);
             }
             $draw .= '
-            context.moveTo(100, 150);
-            context.lineTo(150, 100);
+            context.moveTo(280, 100);
+            context.lineTo(280, 130);
+            context.stroke();';
+            break;
+        case 5:
+            for($count=1;$count<$i;$count++)
+            {
+                $draw .= draw($count);
+            }
+            $draw .= '
+            var x = 280;
+            var y = 160;
+            var radius = 30;
+            var startAngle = 0 * Math.PI;
+            var endAngle = 2 * Math.PI;
+            var counterClockwise = false;
+            context.beginPath();
+            context.arc(x, y, radius, startAngle, endAngle, counterClockwise);
+            context.stroke();';
+            break;
+        case 6:
+            for($count=1;$count<$i;$count++)
+            {
+                $draw .= draw($count);
+            }
+            $draw .= '
+            context.moveTo(280, 190);
+            context.lineTo(280, 330);
+            context.stroke();';
+            break;
+        case 7:
+            for($count=1;$count<$i;$count++)
+            {
+                $draw .= draw($count);
+            }
+            $draw .= '
+            context.moveTo(280, 210);
+            context.lineTo(220, 260);
+            context.stroke();';
+            break;
+        case 8:
+            for($count=1;$count<$i;$count++)
+            {
+                $draw .= draw($count);
+            }
+            $draw .= '
+            context.moveTo(280, 210);
+            context.lineTo(340, 260);
+            context.stroke();';
+            break;
+        case 9:
+            for($count=1;$count<$i;$count++)
+            {
+                $draw .= draw($count);
+            }
+            $draw .= '
+            context.moveTo(280, 330);
+            context.lineTo(220, 380);
+            context.stroke();';
+            break;
+        case 10:
+            for($count=1;$count<$i;$count++)
+            {
+                $draw .= draw($count);
+            }
+            $draw .= '
+            context.moveTo(280, 330);
+            context.lineTo(340, 380);
             context.stroke();';
             break;
     }
+
     return $draw;
 }
 $pagina = '';
-$pagina .= '<script>'.draw(4).'</script>';
+$pagina .= '<script>'.draw(10).'</script>';
 
 echo('<!DOCTYPE HTML>
 <html>
